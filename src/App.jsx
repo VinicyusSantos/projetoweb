fcb2c23bf130d466462583ab5cb0109f11225e26
odeni3 <danielgpmeneses@gmail.com>
2023-08-28 22:44:16 -0300

    adicionando alguns rascunhos à página de login (inicial)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,41 @@
-import { useState } from 'react'
-import hammerLogo from './assets/hammer.svg'
-import './App.css'
+import { useState } from 'react';
+import hammerLogo from './assets/hammer.svg'; // Certifique-se de importar o logo corretamente
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div>
       <div>
-        <a href="" target="_blank">
+        <a href="/" target="_blank" rel="noopener noreferrer">
           <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
         </a>
       </div>
       <h1>AL MOSSAR</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          <div className="degrad">LOGIN</div>
-        </button>
+      <div className="container">
+        <div className="header">
+          <div className="text">Sign Up</div>
+          <div className="underline"></div>
+        </div>
+        <div className="inputs">
+          <div className="input">
+            <input type="email" placeholder="Email"/>
+          </div>
+          <div className="input">
+            <input type="password" placeholder="Password"/>
+          </div>
+        </div>
+        <div className="forgot-password">
+          Lost Password? <span>Click Here!</span>
+        </div>
+        <div className="submit-container">
+          <div className="submit">Sign Up</div>
+          <div className="submit">Login</div>
+        </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/app.js
+++
@@ -1,5 +0,0 @@
-import App from './App.jsx'
-
-function telaprin(){
-    <App/>
-}
