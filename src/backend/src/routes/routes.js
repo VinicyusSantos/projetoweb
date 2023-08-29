76657f7116a10bd05025e2f709f388c2575f38db
Vinicyus Santos <workspace.vinicyus@gmail.com>
2023-08-28 21:58:54 -0300

    criação inicial de obras e visualização especifica

--- src/backend/src/routes/routes.js
+++ src/backend/src/routes/routes.js
@@ -2,18 +2,28 @@ import express from 'express';
 
 export const routes = express.Router()
 
-const obras = [{
-    id: 1,
-    local: 'Parque Recife',
-    custo: '50.000',
-},
-{
-    id: 2,
-    local: 'Escola',
-    custo: '150.000'
-}]
+const obras = [
+    {
+        id: 0,
+        local: 'Parque Recife',
+        custo: '50.000'
+    },
+    {
+        id: 1,
+        local: 'Escola',
+        custo: '150.000'
+    }
+]
 
-routes.get('/obras', (req, res) =>{
-    res.send(obras)
-})
+//visualização de obras especificas
+routes.post('/obras', (req, res) => {
+    const { id } = req.body;
+
+    if (id >= 0 && id < obras.length) {
+        const obra = obras[id];
+        res.status(200).json(obra);
+    } else {
+        res.status(404).json({ error: 'Obra não encontrada' });
+    }
+});
 
