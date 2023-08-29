20ff4e3c386a7f9483f9dfc32ceeaf2658618344
Vinicyus Santos <workspace.vinicyus@gmail.com>
2023-08-28 21:01:30 -0300

    criação variaveis obras

--- src/backend/src/routes/routes.js
+++ src/backend/src/routes/routes.js
@@ -2,6 +2,18 @@ import express from 'express';
 
 export const routes = express.Router()
 
-routes.post('/login', (req, res) =>{
-    res.send('Login endpoint')
+const obras = [{
+    id: 1,
+    local: 'Parque Recife',
+    custo: '50.000',
+},
+{
+    id: 2,
+    local: 'Escola',
+    custo: '150.000'
+}]
+
+routes.get('/obras', (req, res) =>{
+    res.send(obras)
 })
+
