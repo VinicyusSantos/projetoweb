e11ecf6b222eaa391110548e247ca0e11e30e634
odeni3 <danielgpmeneses@gmail.com>
2023-09-05 18:20:54 -0300

    organizando as pastas e outras coisas mais

---
+++ frontend/src/inicialmain.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App2 from './telainicio.jsx'
+
+function testando() {
+    return(
+        ReactDOM.createRoot(document.getElementById('root')).render(
+            <React.StrictMode>
+                <App2 />
+            </React.StrictMode>,
+        )
+    );
+}
+
+export default testando;
---
+++ frontend/src/telalogin.jsx
@@ -0,0 +1,40 @@
+import hammerLogo from './assets/hammer.svg';
+import './telalogin.css';
+import testando from './inicialmain';
+//import Login from './components/Login.jsx';
+
+function App() {
+  return (
+    <div className="app-container">
+      <div>
+        <div>
+          <a href="/" rel="noopener noreferrer">
+            <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
+          </a>
+        </div>
+        <h1 className="centered-h1">AL MOSSAR</h1>
+      </div>
+      <div className="container">
+      <div className="header">
+        <div className="text">Sign In</div>
+        <div className="underline"></div>
+      </div>
+      <div className="inputs">
+        <div className="input">
+          <input type="email" placeholder="Email"/>
+        </div>
+      <div className="input">
+          <input type="password" placeholder="Senha"/>
+        </div>
+      </div>
+          <div className="forgot-password">
+            <span >Esqueci minha senha!</span>
+          </div>
+          <div className="submit-container">
+          <button onClick={testando} className="submit">SIGN IN</button>
+          </div>
+        </div>
+    </div>
+  );
+}
+export default App;
