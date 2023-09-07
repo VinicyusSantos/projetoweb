4c1de7bd224666621a6fedd0a666e29cb04f2918
Vinicyus Santos <workspace.vinicyus@gmail.com>
2023-09-07 14:00:51 -0300

    adicionando rota DELETE nas obras

--- backend/src/routes/obrasRoutes.js
+++ backend/src/routes/obrasRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { obrascontroller } from '../controllers/obrasController.js';
 import { obrasadd } from '../controllers/obrasController.js';
+import { obrasremove } from '../controllers/obrasController.js';
 
 export const obrasRoutes = express.Router()
 
 obrasRoutes.post('/obras', obrascontroller.post);
 obrasRoutes.post('/adicionar', obrasadd.post)
-
+obrasRoutes.delete('/excluir/:id', obrasremove.delete)
