298763414fd49ec5f0735c034862ab1ce3dfed7e
M1guelAndrey <miguelandrey03@gmail.com>
2023-09-08 14:10:33 -0300

    AdmPage base

--- frontend/src/components/AdmPage.jsx
+++ frontend/src/components/AdmPage.jsx
@@ -1,8 +1,24 @@
 // About.js
+import hammerLogo from '../assets/hammer.svg'
 import React from 'react';
+import '../styles/AdmPage.css'
 
 function Adm() {
-  return <div>Sobre Nós</div>;
+  return(
+    <div className="app-container">
+      <div>
+        <div>
+          <a href="/" rel="noopener noreferrer">
+            <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
+          </a>
+        </div>
+        <h1 className="centered-h1">BOA NOITE</h1>
+      </div>
+    </div>
+  )
+  
 }
 
 export default Adm;
+
+
