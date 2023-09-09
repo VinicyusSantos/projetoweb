bae6673d477f345071d09a25965a642f0471482a
Pedro AV <97904358+PedroArvina@users.noreply.github.com>
2023-09-09 11:29:11 -0300

    Update AdmPage.jsx
    Atualização da página central de adm frontend

--- frontend/src/components/AdmPage.jsx
+++ frontend/src/components/AdmPage.jsx
@@ -1,10 +1,18 @@
-// About.js
-import hammerLogo from '../assets/hammer.svg'
-import React from 'react';
-import '../styles/AdmPage.css'
+import React, { useState, useEffect } from 'react';
+import hammerLogo from '../assets/hammer.svg';
+import '../styles/AdmPage.css';
 
-function Adm() {
-  return(
+function AdminPage() {
+  const [works, setWorks] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    // Aqui vai ficar a chamadas do API do backend para obter os dados.
+    // Atualizar tambem os estados 'works', 'employees' e 'orders' com os dados recebidos do backend.
+  }, []);
+
+  return (
     <div className="app-container">
       <div>
         <div>
@@ -12,13 +20,49 @@ function Adm() {
             <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
           </a>
         </div>
-        <h1 className="centered-h1">BOA NOITE</h1>
+        <h1 className="centered-h1">Página Administrativa</h1>
+      </div>
+      <div className="tables-container">
+        <div className="admin-table">
+          <h2>Obras em Andamento</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome da Obra</th>
+                <th>Funcionário envolvido</th>
+                <th>Orçamento</th>
+                <th>Data de conclusão</th>
+                <th>Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Mapeie os dados de 'works' aqui para preencher a tabela */}
+            </tbody>
+          </table>
+        </div>
+        <div className="employee-table">
+          <h2>Funcionários e Obras</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome do Funcionário</th>
+                <th>Obra</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* Mapeie os dados de 'employees' aqui para preencher a tabela */}
+            </tbody>
+          </table>
+        </div>
+        <div className="order-list">
+          <h2>Lista de Pedidos</h2>
+          <ul>
+            {/* Mapeie os dados de 'orders' aqui para preencher a lista de pedidos */}
+          </ul>
+        </div>
       </div>
     </div>
-  )
-  
+  );
 }
 
-export default Adm;
-
-
+export default AdminPage;
