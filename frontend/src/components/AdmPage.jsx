62df8af58dff1daac94eb14b297394f6f3347270
odeni3 <danielgpmeneses@gmail.com>
2023-09-18 20:07:46 -0300

    adicionando pagina do adm (falta algumas poucas coisas)

--- frontend/src/components/AdmPage.jsx
+++ frontend/src/components/AdmPage.jsx
@@ -15,54 +15,21 @@ function AdminPage() {
   }, []);
 
   return (
-    <div className="app-container">
-      <div>
-        <div>
-          <a href="/" rel="noopener noreferrer">
-            <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
-          </a>
-        </div>
+    <div>
+      <div className="app-container">
+        <a href="/" rel="noopener noreferrer">
+          <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
+        </a>
         <h1 className="centered-h1">Página Administrativa</h1>
       </div>
-      <div className="tables-container">
-        <div className="admin-table">
-          <h2>Obras em Andamento</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Nome da Obra</th>
-                <th>Funcionário envolvido</th>
-                <th>Orçamento</th>
-                <th>Data de conclusão</th>
-                <th>Ações</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* Mapeie os dados de 'works' aqui para preencher a tabela */}
-            </tbody>
-          </table>
+      <div className="containerr">
+        <div className="obras">Obras</div>
+        <div className="config">Configurações do site</div>
+        <div className="indicad">Indicadores gráficos</div>
+        <div className="func">Funcionários</div>
+        <div className="solic">Solicitações de funcionários</div>
+        <div className="decis">Decisões</div>
         </div>
-        <div className="employee-table">
-          <h2>Funcionários e Obras</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Nome do Funcionário</th>
-                <th>Obra</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* Mapeie os dados de 'employees' aqui para preencher a tabela */}
-            </tbody>
-          </table>
-        </div>
-        <div className="order-list">
-          <h2>Lista de Pedidos</h2>
-          <ul>
-            {/* Mapear aqui os dados de 'orders' aqui para preencher a lista de pedidos */}
-          </ul>
-        </div>
-      </div>
     </div>
   );
 }
