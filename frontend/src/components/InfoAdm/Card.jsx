35b9b49b40598ef77a6313813e5f4a3a8d9bc6e1
odeni3 <danielgpmeneses@gmail.com>
2023-09-19 19:32:48 -0300

    adicionando pagina de obras do adm

---
+++ frontend/src/components/InfoAdm/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import 'C:/Users/User/Desktop/almossar/projetoweb/frontend/src/styles/Card.css';
+
+function Card({ title, imageUrl, body }) {
+    return (
+        <div className='card-container'>
+            <div className="image-container">
+                <img src={imageUrl} alt='' />
+            </div>
+            <div className="card-content">
+                <div className="card-title">
+                    <h3>{title}</h3>
+                </div>
+                <div className="card-body">
+                    <p>{body}</p>
+                </div>
+            </div>
+            <div className='btn'>
+                <button>
+                    <a>
+                        VEJA MAIS
+                    </a>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
