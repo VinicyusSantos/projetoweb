1262e822dbb46a1b554c4e69e750a8844ba8748d
MarkDosProgramas <mvfranca2004@gmail.com>
2023-09-18 22:17:00 -0300

    adicionando a parte de funcionario

--- frontend/src/components/EmployeePage.jsx
+++ frontend/src/components/EmployeePage.jsx
@@ -1,8 +1,33 @@
-// Home.js
 import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import hammerLogo from '../assets/hammer.svg';
+import '../styles/EmployeePage.css';
 
-export function Employee() {
-  return <div>Página uepa</div>;
+function EmployeePage() {
+  const [works, setWorks] = useState([]);
+  const [employees, setEmployees] = useState([]);
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    // Aqui vai ficar a chamadas do API do backend para obter os dados.
+    // Atualizar tambem os estados 'works', 'employees' e 'orders' com os dados recebidos do backend.
+  }, []);
+
+  return (
+    <div>
+      <div className="app-containerr">
+        <a href="/" rel="noopener noreferrer">
+          <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
+        </a>
+        <h1 className="centered-h11">Página de Funcionário</h1>
+      </div>
+      <div className="containerrr">
+        <div className="obras">Obras</div>
+        <div className="solic">Solicitações de funcionários</div>
+        </div>
+    </div>
+  );
 }
 
-export default Employee;
+export default EmployeePage;
