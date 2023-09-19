dff8b691c8b9d813fa699aea2b3b6c3b0c2287f8
odeni3 <danielgpmeneses@gmail.com>
2023-09-19 01:26:23 -0300

    esqueci do main kk

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,19 +10,6 @@ import Login from "./routes/Login.jsx"
 import Logado from './routes/Logado.jsx';
 import Info from './routes/informacao.jsx';
 
-//rotas temporarias adm
-
-import Decisoes from './components/InfoAdm/Decisoes.jsx'
-import Funcionarios from './components/InfoAdm/Funcionarios.jsx'
-import Indicadores from './components/InfoAdm/Indicadores.jsx'
-import ObrasAdm from './components/InfoAdm/ObrasAdm.jsx'
-import Solicitacoes from './components/InfoAdm/Solicitacoes.jsx'
-
-//rotas temporarias employee
-
-import Obrasfuncionario from './components/InfoEmployee/ObrasFuncionario.jsx'
-import Solicicacoes from './components/InfoEmployee/Solicicacoes.jsx'
-
 
 const router = createBrowserRouter([
   {
