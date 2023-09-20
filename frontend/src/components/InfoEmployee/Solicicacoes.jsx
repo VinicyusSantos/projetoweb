e766fd6f8a46b5ee7bb61f020364e171b414ad9b
MarkDosProgramas <mvfranca2004@gmail.com>
2023-09-20 01:49:53 -0300

    iniciando a pag de solicitações

--- frontend/src/components/InfoEmployee/Solicicacoes.jsx
+++ frontend/src/components/InfoEmployee/Solicicacoes.jsx
@@ -1,7 +1,50 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import '/src/styles/solicitacoesEmployee.css';
+import hammerLogo from '../../assets/hammer.svg';
+
+export function Solicicacoes() {
+  const [solici, setSolici] = useState('');
+  const navigate = useNavigate();
 
 
-export function Solicicacoes(){
-    return <div>solicitacoesfuncionario</div>
+  const handleSubmit = (evento) => {
+    evento.preventDefault(); 
+
+
+    axios.post('/api/enviar', { solici })
+      .then((response) => {
+
+        console.log('Enviado com sucesso:', response.data);
+      })
+      .catch((error) => {
+
+        console.error('Erro ao enviar:', error);
+      });
+  };
+
+  return (
+    <div>
+      <div className="app-container">
+        <a href="/" rel="noopener noreferrer">
+          <img src={hammerLogo} className="logo hammer" alt="Hammer logo" />
+        </a>
+        <h1 className="centered-h1">Digite abaixo sua solicitação</h1>
+      </div>
+      <div className="containerrr">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Digite algo aqui"
+            value={solici}
+            onChange={(evento) => setSolici(evento.target.value)}
+          />
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
-export default Solicicacoes
+
+export default Solicicacoes;
